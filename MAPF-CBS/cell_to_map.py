957da681841ad6bb899ee5af109925503f02a0f9
cheyeon <ppyoo1229@users.noreply.github.com>
2025-07-17 09:51:17 +0900

    상대경로 변경&클라이언트 레이아웃 데이터 생성 완료

--- MAPF-CBS/cell_to_map.py
+++ MAPF-CBS/cell_to_map.py
@@ -6,14 +6,19 @@ MAPF용 변환 기준
 
 -1 : 출고존/입고존 : 시작점, 목표점 : S / G
 """
+import os
 import numpy as np
 import pandas as pd
 
-csv_path = "WarehouseGrid.csv"
-map_path = "warehouse_map.txt"
 
-# CSV 불러오기 (구분자가 tab 또는 \t 혹은 \s+ 인 경우 sep 지정)
-data = pd.read_csv(csv_path, sep='\t', header=None)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+csv_path = os.path.join(BASE_DIR, "data", "WarehouseGrid.csv")
+save_dir = os.path.join(BASE_DIR, "data")
+os.makedirs(save_dir, exist_ok=True)
+
+save_path = os.path.join(save_dir, "map.txt")
+
+data = pd.read_csv(csv_path, sep=',', header=None, encoding='utf-8')
 
 # 맵 변환 함수
 def cell_to_char(cell):
@@ -26,12 +31,13 @@ def cell_to_char(cell):
 char_map = data.applymap(cell_to_char).values
 
 # 맵 크기
+#char_map = data.applymap(cell_to_char).values
 rows, cols = char_map.shape
 
-# 맵 파일 저장 (MAPF 오픈소스 포맷)
-with open(map_path, 'w') as f:
+# 저장
+with open(save_path, 'w', encoding='utf-8') as f:
     f.write(f"{rows} {cols}\n")
     for row in char_map:
         f.write(''.join(row) + '\n')
 
-print(f"변환 완료 {map_path} 저장")
+print(f"변환 완료 : {save_path}")
