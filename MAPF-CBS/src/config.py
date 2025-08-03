2063af2e66b1751023384b4c91dbab1d865613f2
cheyeon <ppyoo1229@users.noreply.github.com>
2025-08-03 20:09:43 +0900

    config 정리

--- MAPF-CBS/src/config.py
+++ MAPF-CBS/src/config.py
@@ -1,4 +1,3 @@
-# src/config.py
 import os
 
 class Config:
@@ -7,9 +6,8 @@ class Config:
         self.BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         self.DATA_DIR = os.path.join(self.BASE_DIR, "data")
         self.RESULTS_DIR = os.path.join(self.BASE_DIR, "results")
-
         self.MAP_PATH = os.path.join(self.DATA_DIR, "map.csv")
-        
+
         # 에이전트/실험 파라미터
         self.N_AGENTS = 1
         self.AGENT_NAMES = [f"AGV_{i+1}" for i in range(self.N_AGENTS)]
@@ -21,10 +19,11 @@ class Config:
         self.EXPERIMENT_NAME = "exp01_baseline"
 
         # 시각화/저장 옵션
-        self.SAVE_FIG = True
-        self.SAVE_ANIMATION = True
-        self.ANIMATION_INTERVAL = 300
-        self.SHOW_COLLISIONS = True
+        self.SAVE_FIG = True            # 경로 시각화 이미지 저장 여부
+        self.SAVE_ANIMATION = True      # 경로 애니메이션 저장 여부
+        self.ANIMATION_INTERVAL = 300   # 애니메이션 프레임 간격(ms)
+        self.SHOW_COLLISIONS = True     # 충돌 표시 여부
+        self.FIG_DPI = 150              # 그림 저장시 해상도
 
         # 강화학습 파라미터
         # self.RL_ALGO = "ppo"
@@ -32,20 +31,26 @@ class Config:
         # self.RL_GAMMA = 0.99
         # self.RL_BATCH_SIZE = 64
 
-        # 배치 실험 관리(여러 세트 돌릴 경우)
-        self.EXPERIMENTS = [
-            {
-                "name": "exp01_cbs_manhattan",
-                "n_agents": 8,
-                "heuristic": "manhattan",
-                "map_file": self.MAP_PATH,
-                "task_file": self.TASK_PATH
-            },
-        
-        ]
+        # 배치 실험
+        # self.EXPERIMENTS = [
+        #     {
+        #         "name": "exp01_cbs_manhattan",
+        #         "n_agents": 8,
+        #         "heuristic": "manhattan",
+        #         "map_file": self.MAP_PATH,
+        #         # "task_file": self.TASK_PATH
+        #     },
+        # ]
 
-    def get_result_path(self, filename):
-        return os.path.join(self.RESULTS_DIR, filename)
+    def get_result_path(self, prefix="fig", ext=".png", loop=None):
+        """
+        실험 결과 파일명 자동화 (prefix/확장자/loop 등 주요 변수 포함)
+        """
+        fname = f"{prefix}_{self.N_AGENTS}_{self.MAX_EPISODES}_{self.MAX_TIMESTEP}"
+        if loop is not None:
+            fname += f"_loop{loop}"
+        fname += ext
+        return os.path.join(self.RESULTS_DIR, fname)
 
     def __repr__(self):
         return str(self.__dict__)
